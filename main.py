aea743a1577669a0a1348434b30cca148d016b58
Gianello Montenegro <gianello.montenegro@eit.edu.au>
2024-11-23 19:44:35 +0800

    add main.py for image processing and update .gitignore to exclude unnecessary files

---
+++ main.py
@@ -0,0 +1,21 @@
+import opencv as cv
+import numpy as np
+import os
+from detect import run
+
+sample_images_path = 'data/images'
+weights_path = 'best.pt'
+
+def main():
+    for image in os.listdir(sample_images_path):
+        img = cv.imread(os.path.join(sample_images_path, image))
+        results = run(weights=weights_path, source=img)
+        
+        for result in results:
+            # preview the results
+            cv.imshow('result', result)
+            cv.waitKey(0)
+            cv.destroyAllWindows()
+            
+if __name__ == '__main__':
+    main()
